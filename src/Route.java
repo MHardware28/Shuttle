c4027b4b8e2cc1b37b3a4c5e5e7f76c4ce4ba09f
Sara-Sid <sara.brown.2005@outlook.com>
2025-12-03 14:50:26 -0500

    Add documentation comments to Route class
    Added class-level documentation for Route.

--- src/Route.java
+++ src/Route.java
@@ -1,12 +1,14 @@
 import java.util.List;
-
+/*
+  Represents a shuttle route, which is made up of a list of stops.
+ */
 public class Route {
     protected List<Stop> stops;
-
+// Creates a route using the list of stops provided 
     public Route(List<Stop> stops){
         this.stops = stops;
     }
-
+//  Returns the list of stops in this route.
     public List<Stop> getStops(){
         return stops;
     }
