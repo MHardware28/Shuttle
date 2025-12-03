4dec6c02e5e029a717b59f4389084c147cab3787
Sara-Sid <sara.brown.2005@outlook.com>
2025-12-03 14:27:14 -0500

    Enhance Stop class with author and documentation comments
    Added author comment and documentation for Stop class.

--- src/Stop.java
+++ src/Stop.java
@@ -1,6 +1,12 @@
+// Author: Sara-Lee Brown 
+/*
+Represents a single stop along a shuttle route.
+*/
 public class Stop {
     protected String name;
-
+/* Creates a new stop 
+    Returns the name of the stop
+*/
     public Stop(String name) {
         this.name = name;
     }
