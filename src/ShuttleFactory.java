2aedc0cc49a8ced508201b9c696260ec1862783b
Sara-Sid <sara.brown.2005@outlook.com>
2025-12-03 14:46:59 -0500

    Enhance comments in ShuttleFactory.java
    Added comments to clarify the purpose of the ShuttleFactory class and its methods.

--- src/ShuttleFactory.java
+++ src/ShuttleFactory.java
@@ -1,12 +1,16 @@
+// Author: Sara-Lee Brown 
 import java.util.ArrayList;
 import java.util.List;
-
+/*  Creates a ShuttleService based on the specified shuttle type.
+Factory Method is used to select and create the appropriate concrete shuttle creator (Day, Night, or Weekend) at runtime.
+*/  
 public class ShuttleFactory {
 
     public static ShuttleService createShuttle(ShuttleType type) {
 
         ShuttleCreator creator;
-
+// Choose the shuttle to make depending on the type 
+        
         switch (type) {
             case DAY:
                 creator = new DayShuttleCreator();
@@ -18,6 +22,7 @@ public class ShuttleFactory {
                 creator = new WeekendShuttleCreator();
                 break;
             default:
+                // If the type does not match any known case, throw an error
                 throw new IllegalArgumentException("Unknown shuttle type " + type);
         }
 
