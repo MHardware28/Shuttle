1ae11a037ab4dd7f4cc7228fd951ab38237ae64f
Sara-Sid <sara.brown.2005@outlook.com>
2025-12-03 14:20:51 -0500

    Implement Singleton pattern in ShuttleService
    Refactor ShuttleService class to implement Singleton pattern and manage the current shuttle Route.

--- src/ShuttleService.java
+++ src/ShuttleService.java
@@ -1,3 +1,7 @@
+/*
+ShuttleService implements a Singleton pattern that manages the current shuttle Route.
+Only one wants one instance of a shuttle.
+*/
 public class ShuttleService {
     private static ShuttleService instance;
     protected Route route;
@@ -5,25 +9,33 @@ public class ShuttleService {
 
     private ShuttleService(){
     }
+    
+   /* Returns the single instance of ShuttleService.
+      If it does not exist yet, it creates one.
+    */
+     
     public static ShuttleService getInstance(){
         if(instance == null) {
             instance = new ShuttleService();
         }
         return instance;
     }
+    
+    /* 
+    Set the current route 
+    Return the current route 
+    Clears the current route, resetting the service to its initial state.
+    */
 
     public void setRoute(Route route){
         this.route = route;
     }
-
     public Route getRoute(){
-
         return route;
     }
     public void resetRoute() {
         this.route = null;
     }
-
         return route;
     }
 
