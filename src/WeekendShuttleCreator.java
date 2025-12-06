26719cefe59fb6dee8067fd6f87f5fc728a5cc6d
codequeen-debug <r.campbell27@ncf.edu>
2025-12-05 20:58:05 -0500

    Modify WeekendShuttleCreator to set weekend route
    Updated the WeekendShuttleCreator to reset the route and set a weekend stop list.

--- src/WeekendShuttleCreator.java
+++ src/WeekendShuttleCreator.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeekendShuttleCreator extends ShuttleCreator {
+
     @Override
     public ShuttleService createShuttle() {
         ShuttleService shuttle = ShuttleService.getInstance();
-        shuttle.setRoute(null);   //No fixed route
+        shuttle.resetRoute();
+
+        //weekend stop list
+        Route route = new Route(List.of(
+                new Stop("Home2Suites Hotel"),
+                new Stop("USF Atala Hall"),
+                new Stop("NCF Sudakoff"),
+                new Stop("Marine Bio Building")
+        ));
+
+        shuttle.setRoute(route);
         return shuttle;
     }
 }
