edcfdd1f7d53546df76d5de137dfcfcca9faaf52
codequeen-debug <r.campbell27@ncf.edu>
2025-12-05 19:26:32 -0500

    Update status message in enroute method

--- src/LeavingState.java
+++ src/LeavingState.java
@@ -7,7 +7,7 @@ public class LeavingState implements State{
     @Override
     public void enroute(ShuttleContext context, String shuttleLocation){
         context.setState(new EnrouteState());
-        context.updateStatus("Shuttle is enroute to: " + shuttleLocation, 5.0);
+        context.updateStatus("- Pick up round completed - Shuttle Leaving: " + shuttleLocation, 5.0);
     }
     @Override
     public void isArriving(ShuttleContext context, String shuttleLocation){
